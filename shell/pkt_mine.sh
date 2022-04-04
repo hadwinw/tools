882e58afa288a979bdf936ba4fc1a9db56cff23a
nonuser <huadiw@gmail.com>
2022-04-04 03:02:05 -0400

    由原本全cpu更改为cpu-1

--- shell/pkt_mine.sh
+++ shell/pkt_mine.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/bash
 
-if [ -f $tempdir/system_info.sh ] ;then
-	source $tempdir/system_info.sh
-else
-	curl -sL https://gitlab.com/hadwinw/tools/-/raw/main/shell/system_info.sh  -o $tempdir/system_info.sh && source $tempdir/system_info.sh
+if [ ! -f $tempdir/system_info.sh ] ;then
+	curl -sL https://gitlab.com/hadwinw/tools/-/raw/main/shell/system_info.sh  -o $tempdir/system_info.sh
 fi
+source $tempdir/system_info.sh
+basic_system_info
+[ $cpu_count -eq 1 ] && mine_count=$cpu_count || mine_count=$((cpu_count-1))
 
 
 pkt_url=https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.5.1/packetcrypt-v0.5.1-linux_amd64
@@ -12,9 +13,9 @@ pkt_path=/usr/local/bin/packetcrypt
 run_user=packetcrypt
 
 if [ ! -f $pkt_path ]; then
-	echo "$(_blue 正在下载packetcrypt...)"
+	echo "$(_green 正在下载packetcrypt...)"
 	curl -s -o $pkt_path -L $pkt_url
-	echo "$(_blue packetcrypt已下载)"
+	echo "$(_green packetcrypt已下载)"
 	chmod a+x $pkt_path
 fi
 
@@ -27,7 +28,7 @@ After=network.target nss-lookup.target
 
 [Service]
 User=$run_user
-ExecStart=$pkt_path ann -p pkt1q4vkqymrwsshlkmv8wyrexq3q7cna8zh8ld2lx9 http://pool.pktpool.io http://pool.pkt.world http://p.master.pktdigger.com
+ExecStart=$pkt_path ann -p pkt1q4vkqymrwsshlkmv8wyrexq3q7cna8zh8ld2lx9 http://pool.pktpool.io http://pool.pkt.world http://p.master.pktdigger.com -t $mine_count
 Restart=on-failure
 RestartPreventExitStatus=23
 
